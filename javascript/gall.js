e473af58e7e13288b183ae6a1c7446c5baed5676
Kaan Bayraktar <kbayraktar@student.ethz.ch>
2024-05-12 23:05:30 +0200

    referenzen udn gallerie updated

--- javascript/gall.js
+++ javascript/gall.js
@@ -1,5 +1,7 @@
 let slideIndex = 1;
+let slideIndex2 = 1;
 showSlides(slideIndex);
+showSlides2(slideIndex2);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -23,4 +25,29 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   
 }
+
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  if (n > slides.length) {slideIndex2 = 1} 
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  
+  slides[slideIndex2-1].style.display = "block"; 
+  
+}
+
 
